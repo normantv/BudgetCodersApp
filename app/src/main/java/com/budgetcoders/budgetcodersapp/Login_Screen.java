85124b4c45b69a221a8be420b0fff84c520aa709
Justin <jles@bu.edu>
2018-12-06 11:47:42 -0500

    Version With GUI

---
+++ app/src/main/java/com/budgetcoders/budgetcodersapp/Login_Screen.java
@@ -0,0 +1,35 @@
+package com.budgetcoders.budgetcodersapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+
+public class Login_Screen extends AppCompatActivity {
+    private Button toAccountCreate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__screen);
+
+        toAccountCreate = (Button) findViewById(R.id.button2);
+
+
+        toAccountCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToAccountCreate();
+                finish();
+            }
+        });
+
+    }
+
+
+    private void goToAccountCreate() {
+
+        Intent intent = new Intent(this, Account_Creation.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/budgetcoders/budgetcodersapp/MainActivity.java
+++ app/src/main/java/com/budgetcoders/budgetcodersapp/MainActivity.java
@@ -3,6 +3,45 @@ package com.budgetcoders.budgetcodersapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button ToLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ToLogin = (Button) findViewById(R.id.button);
+
+        ToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                goToLogin();
+            }
+        });
+
+    }
+    private void goToLogin() {
+
+        Intent intent = new Intent(this, Login_Screen.class);
+        startActivity(intent);
+    }
+}
+
+
+
+
+
+
+
+
+
+/*
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
@@ -17,6 +56,9 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("goals");
     }
+
+
+
 
 
     @Override
@@ -48,4 +90,5 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+*/
 
