790998b184c9ed2abedfddf4fea9d54111313f9a
natapat chotruchsakul <pnc491@bu.edu>
2018-12-08 18:00:30 -0500

    Account creation almost working

--- app/src/main/java/com/budgetcoders/budgetcodersapp/Account_Creation.java
+++ app/src/main/java/com/budgetcoders/budgetcodersapp/Account_Creation.java
@@ -3,22 +3,47 @@ package com.budgetcoders.budgetcodersapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.content.Intent;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Account_Creation extends AppCompatActivity {
+
     private Button createAccount;
+    private EditText Password;
+    private EditText Email;
+    private EditText FirstName;
+    private EditText LastName;
+
+    Backend_UsersDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account__creation);
 
-        createAccount = findViewById(R.id.button3);
+        db = Backend_UsersDatabase.getDatabase(this);
+
+        FirstName = (EditText)findViewById(R.id.FName);
+        LastName = (EditText)findViewById(R.id.LName);
+        Password = (EditText)findViewById(R.id.PW);
+        Email = (EditText)findViewById(R.id.Email);
+
+        createAccount = findViewById(R.id.CreateAccount);
 
         createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String FirstNamestring = FirstName.getText().toString();
+                String LastNamestring = LastName.getText().toString();
+                String Passwordstring = Password.getText().toString();
+                String Emailstring = Email.getText().toString();
+
+                Backend_UserProfile me = new Backend_UserProfile(FirstNamestring, LastNamestring, Emailstring, Passwordstring);
+
+                db.Backend_UserDAO().insert(me);
+
+
                 finish();
             }
         });
